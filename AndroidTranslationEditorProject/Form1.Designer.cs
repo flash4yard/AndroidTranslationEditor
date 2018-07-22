1dcf4469a4cc902366afd9e5dee334e3b9aab5ea
Fabian <digafabian@gmail.com>
2018-07-22 21:11:13 +0200

    Initial commit with main functionality

--- AndroidTranslationEditorProject/Form1.Designer.cs
+++ AndroidTranslationEditorProject/Form1.Designer.cs
@@ -28,13 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.table = new System.Windows.Forms.DataGridView();
+            this.C1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fileOutput = new System.Windows.Forms.SaveFileDialog();
+            this.fileInput = new System.Windows.Forms.OpenFileDialog();
+            this.toolBar = new System.Windows.Forms.ToolStrip();
+            this.OpenFile = new System.Windows.Forms.ToolStripButton();
+            this.CreateFile = new System.Windows.Forms.ToolStripButton();
+            this.About = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.toolBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.C1});
+            this.table.Location = new System.Drawing.Point(0, 28);
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(800, 419);
+            this.table.TabIndex = 0;
+            this.table.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.CellUpdated);
+            // 
+            // C1
+            // 
+            this.C1.HeaderText = "Key";
+            this.C1.Name = "C1";
+            this.C1.ReadOnly = true;
+            // 
+            // fileInput
+            // 
+            this.fileInput.FileName = "openFileDialog1";
+            // 
+            // toolBar
+            // 
+            this.toolBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenFile,
+            this.CreateFile,
+            this.About});
+            this.toolBar.Location = new System.Drawing.Point(0, 0);
+            this.toolBar.Name = "toolBar";
+            this.toolBar.Size = new System.Drawing.Size(800, 25);
+            this.toolBar.TabIndex = 1;
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.OpenFile.Image = ((System.Drawing.Image)(resources.GetObject("OpenFile.Image")));
+            this.OpenFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.OpenFile.Name = "OpenFile";
+            this.OpenFile.Size = new System.Drawing.Size(23, 22);
+            this.OpenFile.Text = "&Open";
+            this.OpenFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
+            // CreateFile
+            // 
+            this.CreateFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.CreateFile.Image = ((System.Drawing.Image)(resources.GetObject("CreateFile.Image")));
+            this.CreateFile.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.CreateFile.Name = "CreateFile";
+            this.CreateFile.Size = new System.Drawing.Size(23, 22);
+            this.CreateFile.Text = "&New";
+            this.CreateFile.Click += new System.EventHandler(this.CreateFile_Click);
+            // 
+            // About
+            // 
+            this.About.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.About.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.About.Image = ((System.Drawing.Image)(resources.GetObject("About.Image")));
+            this.About.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.About.Name = "About";
+            this.About.Size = new System.Drawing.Size(23, 22);
+            this.About.Text = "He&lp";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolBar);
+            this.Controls.Add(this.table);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.toolBar.ResumeLayout(false);
+            this.toolBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C1;
+        private System.Windows.Forms.SaveFileDialog fileOutput;
+        private System.Windows.Forms.OpenFileDialog fileInput;
+        private System.Windows.Forms.ToolStrip toolBar;
+        private System.Windows.Forms.ToolStripButton CreateFile;
+        private System.Windows.Forms.ToolStripButton OpenFile;
+        private System.Windows.Forms.ToolStripButton About;
     }
 }
 
--- AndroidTranslationEditorProject/Form1.cs
+++ AndroidTranslationEditorProject/Form1.cs
@@ -1,20 +1,218 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace AndroidTranslationEditor
 {
     public partial class Form1 : Form
     {
+
+        List<List<XMLValue>> LanguageLists = new List<List<XMLValue>>();
+        List<String> keyList = new List<string>();
+        List<String> openFiles = new List<string>();
+        Boolean loadingFile = true;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ApplyDesign();
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            fileDialog.Filter = "XML files|*.xml";
+            fileDialog.ShowDialog();
+
+            if (fileDialog.FileName != "")
+            {
+                loadingFile = true;
+                AddFileEntry(fileDialog.FileName, fileDialog.SafeFileName);
+                loadingFile = false;
+            }           
+        }
+
+        private void CreateFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog fileDialog = new SaveFileDialog();
+
+            fileDialog.Filter = "XML files|*.xml";
+            fileDialog.ShowDialog();
+
+            string path = fileDialog.FileName;
+
+            if (path != "")
+            {
+                FileStream file = System.IO.File.Create(path);
+                Byte[] emptyXMLstring = new UTF8Encoding().GetBytes("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<resources>\n</resources>");
+
+                file.Write(emptyXMLstring, 0, emptyXMLstring.Length);
+                file.Close();
+
+                loadingFile = true;
+                AddFileEntry(path, System.IO.Path.GetFileName(path));
+                loadingFile = false;
+            }
+        }
+
+        public void AddFileEntry(string path, string name)
+        {
+            LanguageLists.Add(XMLprocessing.XMLRead(path));
+            openFiles.Add(path);
+            FillTable(path, name);
+        }
+
+        public void FillTable(string path, string name)
+        {
+            table.Visible = true;
+            table.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
+
+            int activeColumn = table.Columns.Add(path, name);
+
+            foreach(XMLValue current in LanguageLists[activeColumn-1])
+            {
+                if(keyList.Contains(current.StringName))
+                {
+                    int cIndex = keyList.IndexOf(current.StringName);
+                    table.Rows[cIndex].Cells[activeColumn].Value = current.StringText;
+                }
+                else
+                {
+                    //Add new row
+                    keyList.Add(current.StringName);
+                    int currentIndex = table.Rows.Add(current.StringName);
+                    table.Rows[currentIndex].Cells[activeColumn].Value = current.StringText;
+
+                    //If not translatable
+
+                    if(!current.StringTranslateable)
+                    {
+                        table.Rows[currentIndex].ReadOnly = true;
+                        table.Rows[currentIndex].DefaultCellStyle.BackColor = Color.LightGray;
+                    }
+                }
+            }
+
+            table.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+
+        private void CellUpdated(object sender, DataGridViewCellEventArgs e)
+        {
+            if(!loadingFile)
+            {
+                XMLprocessing.XMLWrite(openFiles[e.ColumnIndex - 1], keyList[e.RowIndex], (string)table.Rows[e.RowIndex].Cells[e.ColumnIndex].Value);
+            }        
+        }
+
+        private void ApplyDesign()
+        {
+            //Permissions
+            table.AllowUserToAddRows = false;
+            table.AllowUserToDeleteRows = false;
+            table.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            table.MultiSelect = false;
+            table.AllowUserToResizeColumns = true;
+
+            //Styles
+
+            table.BorderStyle = BorderStyle.Fixed3D;
+            table.DefaultCellStyle.SelectionBackColor = Color.White;
+            table.DefaultCellStyle.SelectionForeColor = Color.Blue;
+
+            Font columnFont = new Font(FontFamily.GenericSansSerif, 8, FontStyle.Bold);
+            table.ColumnHeadersDefaultCellStyle.Font = columnFont;
+
+            // Wrap
+            table.DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            table.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+
+            table.AllowDrop = false;
+            table.AllowUserToOrderColumns = false;
+
+            table.CellBorderStyle = DataGridViewCellBorderStyle.Raised;
+            table.RowHeadersVisible = false;
+
+            // lag compensation
+            DoubleBuffered = true;
+        }   
+                      
+    }
+
+    public class XMLprocessing
+    {
+        public static List<XMLValue> XMLRead(string path)
+        {
+            XmlDocument XMLRead = new XmlDocument();
+            List<XMLValue> values = new List<XMLValue>();
+
+            XMLRead.Load(path);
+
+            foreach (XmlNode node in XMLRead.SelectNodes("/resources/string"))
+            {
+                XMLValue currentValue = new XMLValue();
+
+                currentValue.StringText = node.InnerText;
+
+                foreach (XmlAttribute attribute in node.Attributes)
+                {
+                    switch (attribute.Name)
+                    {
+                        case "name":
+                            currentValue.StringName = attribute.Value;
+                            break;
+
+                        case "translatable":
+                            currentValue.StringTranslateable = Boolean.Parse(attribute.Value);
+                            break;
+                    }
+                }
+                values.Add(currentValue);
+            }
+            return values;
+        }
+
+        public static void XMLWrite(string path, string id, string value)
+        {
+            XmlDocument XMLWrite = new XmlDocument();
+
+            XMLWrite.Load(path);
+
+            if (XMLWrite.SelectSingleNode("/resources/string[@name='" + id + "']") != null)
+            {
+                XMLWrite.SelectSingleNode("/resources/string[@name='" + id + "']").InnerText = value;
+            }
+            else
+            {
+                XmlNode newItem = XMLWrite.CreateElement("string");
+                newItem.InnerText = value;
+
+                XmlAttribute newAttr = XMLWrite.CreateAttribute("name");
+                newAttr.InnerText = id;
+
+                newItem.Attributes.Append(newAttr);
+
+                XMLWrite.SelectSingleNode("/resources").AppendChild(newItem);
+
+            }
+            XMLWrite.Save(path);
+        }
+    }
+
+    public class XMLValue
+    {
+        public String StringName;
+        public String StringText;
+        public Boolean StringTranslateable = true;
     }
 }
