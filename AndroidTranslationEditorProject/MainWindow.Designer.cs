40f30d9698d5bafb8b0d24a6e82224ca28df5de3
Fabian <digafabian@gmail.com>
2018-07-23 21:10:45 +0200

    Added Icon

--- AndroidTranslationEditorProject/MainWindow.Designer.cs
+++ AndroidTranslationEditorProject/MainWindow.Designer.cs
@@ -109,14 +109,15 @@
             this.About.Size = new System.Drawing.Size(23, 22);
             this.About.Text = "He&lp";
             // 
-            // Form1
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.toolBar);
             this.Controls.Add(this.table);
-            this.Name = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
             this.Text = "Android Translation Editor";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
